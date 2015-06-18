d26dfde46b8d76b9640eef1da9b1d5bb253f2f57
Pedro Lopes <hello@pedrolopes.net>
2015-06-18 15:58:02 +0100

    Layout updates and sample inputs improvements.

--- data/inputs/rss.js
+++ data/inputs/rss.js
@@ -1,7 +1,7 @@
 {
     "identifier": "svbtle_rss",
-    "title": "RSS Feed",
-    "help": "Monitor content from RSS feed.",
+    "title": "Feed",
+    "help": "Monitor content from RSS feed. Beware that there are multiple feed standards (RSS, RSS2, Atom...), make sure that these settings match your input data.",
     "publisher": "xml",
     "schedule": "5m",
     "payload": {
